ac21d519547b3acd72d292a7d057d6081c18b6c1
kirschn <github@kirschn.de>
2016-08-12 23:16:00 +0200

    Added Spotify Installation

--- install.sh
+++ install.sh
@@ -11,7 +11,19 @@ sudo apt-get update
 rm Release.key
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
 sudo apt-get update
-sudo apt-get install arc-theme
+sudo apt-get install arc-theme --yes
+# 1. Add the Spotify repository signing key to be able to verify downloaded packages
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+
+# 2. Add the Spotify repository
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+# 3. Update list of available packages
+sudo apt-get update
+
+# 4. Install Spotify
+sudo apt-get install spotify-client
+
 
 
 
